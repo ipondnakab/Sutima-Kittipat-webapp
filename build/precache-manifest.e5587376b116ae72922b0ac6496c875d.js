eaee866f814d209024c3a0f27dcbb1dc8dc1ca1b
ipondnakab <kittipat_dd@kkumail.com>
2020-09-30 04:25:45 +0700

    Page Public and component post

---
+++ build/precache-manifest.e5587376b116ae72922b0ac6496c875d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101713c2e303db2ee39ed148ab93c8d1",
+    "url": "/Sutima-Kittipat-webapp/index.html"
+  },
+  {
+    "revision": "d4d9b02281c8170ab0fe",
+    "url": "/Sutima-Kittipat-webapp/static/css/main.28147bb3.chunk.css"
+  },
+  {
+    "revision": "10e68805d0968eb5d6ec",
+    "url": "/Sutima-Kittipat-webapp/static/js/2.6c89d746.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sutima-Kittipat-webapp/static/js/2.6c89d746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d9b02281c8170ab0fe",
+    "url": "/Sutima-Kittipat-webapp/static/js/main.3e39a01d.chunk.js"
+  },
+  {
+    "revision": "1472a5a9e6a1645cdf44",
+    "url": "/Sutima-Kittipat-webapp/static/js/runtime-main.e1a4823c.js"
+  },
+  {
+    "revision": "2f46140e19bde069cb2b19bbace111fd",
+    "url": "/Sutima-Kittipat-webapp/static/media/bg2.2f46140e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sutima-Kittipat-webapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ef68baeea72fc1feef9e79f173739972",
+    "url": "/Sutima-Kittipat-webapp/static/media/man.ef68baee.svg"
+  }
+]);
