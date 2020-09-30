05018cea3dc69cdd2116b24ac7797ebd9f1feaea
ipondnakab <kittipat_dd@kkumail.com>
2020-09-30 13:01:13 +0700

    Deploy for public

---
+++ build/precache-manifest.daf600c7b37bc49ec3c256b26b1ed1c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20da37723e47aea5c80e8d2cb3308a2e",
+    "url": "/Sutima-Kittipat-webapp/index.html"
+  },
+  {
+    "revision": "05858c31d244b9fc4b98",
+    "url": "/Sutima-Kittipat-webapp/static/css/main.93bbc221.chunk.css"
+  },
+  {
+    "revision": "304ac9490897ac212edc",
+    "url": "/Sutima-Kittipat-webapp/static/js/2.3eae4863.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Sutima-Kittipat-webapp/static/js/2.3eae4863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05858c31d244b9fc4b98",
+    "url": "/Sutima-Kittipat-webapp/static/js/main.f64c6d85.chunk.js"
+  },
+  {
+    "revision": "1472a5a9e6a1645cdf44",
+    "url": "/Sutima-Kittipat-webapp/static/js/runtime-main.e1a4823c.js"
+  },
+  {
+    "revision": "2f46140e19bde069cb2b19bbace111fd",
+    "url": "/Sutima-Kittipat-webapp/static/media/bg2.2f46140e.jpg"
+  },
+  {
+    "revision": "ef68baeea72fc1feef9e79f173739972",
+    "url": "/Sutima-Kittipat-webapp/static/media/man.ef68baee.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3bd62564e627593f29641f4ac08984c.js"
+  "/Sutima-Kittipat-webapp/precache-manifest.daf600c7b37bc49ec3c256b26b1ed1c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sutima-Kittipat-webapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
