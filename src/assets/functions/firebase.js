fabc7602261bd86a03f4facb83e2e83172b53953
ipondnakab <kittipat_dd@kkumail.com>
2020-11-03 03:38:31 +0700

    add function login/register with email/password

---
+++ src/assets/functions/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBlkL_X4nWqW5Y1TIsDsQZnFOvgyfkKdZM",
+  authDomain: "farmacro-af287.firebaseapp.com",
+  databaseURL: "https://farmacro-af287.firebaseio.com",
+  projectId: "farmacro-af287",
+  storageBucket: "farmacro-af287.appspot.com",
+  messagingSenderId: "964427814223",
+  appId: "1:964427814223:web:8660f5af152ff5299da13a",
+  measurementId: "G-78M5EZYR9J",
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
